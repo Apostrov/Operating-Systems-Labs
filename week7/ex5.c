1ba17bca4dfd11663235607b583faa296a2cab33
Apostrov <amir.subaev@gmail.com>
2018-10-03 17:10:18 +0300

    Week 7 fix 5 exercise

--- week7/ex5.c
+++ week7/ex5.c
@@ -5,7 +5,7 @@ int main() {
     char **s = malloc(0);
     char foo[] = "Hello World";
     *s = foo; 
-    printf("s is %s\n",s);
+    printf("s is %s\n",*s);
     s[0] = foo;
     printf("s[0] is %s\n",s[0]);
     return(0);
